37e3ece76d2ccf77c3577f4c022a292ea11fa2fe
RubenC2 <rubenct1993@gmail.com>
2024-09-27 00:04:51 +0200

    fotos, gifs y form acabado

---
+++ Pages/script.js
@@ -0,0 +1,13 @@
+
+// document.getElementsByClassName("buttonForm").addEventListener("submit", function() {
+//     const mensaje = "Mensaje enviado, muchas gracias."
+// })
+
+const formulario = document.querySelector('.contactForm');
+
+formulario.addEventListener('submit', function (event) {
+    alert('Mensaje enviado, muchas gracias.');
+})
+
+
+
